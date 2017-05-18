da962b2e0477aca71c2b6c824394b79837448633
itelios <itelios@drupaldummy.dev>
2017-05-18 11:05:20 -0300

    Template básico para Drupal 8

---
+++ gulp/browserSync.js
@@ -0,0 +1,13 @@
+'use strict';
+import gutil from 'gulp-util';
+export default function(gulp, plugins, args, config, taskTarget, browserSync) {
+
+  // BrowserSync
+  gulp.task('browserSync', () => {
+		browserSync.init(['./css/*.css', './js/build/*.js'], {
+		  proxy: "drupaldummy.docksal",
+		  port: config.port || 3000,
+      open: args.open ? 'local' : false,
+		});
+  });
+}
